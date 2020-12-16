ea405717a07cd84dbc56310234f7a7747c52640e
zakiyagarana27 <nasimkze@gmail.com>
2020-12-15 20:18:42 -0500

    checking

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,5 +4,10 @@ import "../static/css/styles.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return( 
+    <>
+    
+  <Component {...pageProps} />
+  </>
+  );
 }
